5e1c263313caa876a981550a60209b0df3eb2cb8
Nhuune <84001638+Nhuune@users.noreply.github.com>
2022-04-08 17:46:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   
+      <Product_detail/>
     </div>
   );
 }
--- src/component/product_detail.jsx
+++ src/component/product_detail.jsx
@@ -1,57 +1,56 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './style/index.css';
-import { Layout, Carousel } from 'antd';
+import { Layout, Slider } from 'antd';
+import Involve from './involve';
 import Mockdata from './mockdata';
 import NoteCmt from './notecmt';
+import Slide from './slider';
+
 const { Header, Footer, Sider, Content } = Layout;
 
-ReactDOM.render(
-  <div>
-    <Layout>
-    <Header>Header</Header>
-      <h1>
-        <p>Cho thuê phòng trọ chung cư có máy lạnh giá rẻ</p>
-      </h1>
-      <Carousel autoplay>
-        <div>
-          <img
-            src={'https://www.ezdanhotels.qa/wp-content/uploads/2018/10/1.png'}
-          />
-        </div>
-        <div>
-          <img
-            src={
-              'https://th.bing.com/th/id/R.c5fc7178de44add0a27f15bcaf6e4d72?rik=lpF%2bNLm7WphDCQ&riu=http%3a%2f%2fwww.grandlordhotel.com%2fmedias%2froom%2fbig%2f81%2fdux-corner-03.jpg&ehk=5J9kzRaVXH%2bvO4s5MDr1rlopJpFl%2bvlGj6Xeekt8fA8%3d&risl=&pid=ImgRaw&r=0'
-            }
-          />
-        </div>
-        <div>
-          <img
-            src={'https://www.ezdanhotels.qa/wp-content/uploads/2018/10/1.png'}
-          />
-        </div>
-        <div>
-          <img
-            src={
-              'https://th.bing.com/th/id/R.c5fc7178de44add0a27f15bcaf6e4d72?rik=lpF%2bNLm7WphDCQ&riu=http%3a%2f%2fwww.grandlordhotel.com%2fmedias%2froom%2fbig%2f81%2fdux-corner-03.jpg&ehk=5J9kzRaVXH%2bvO4s5MDr1rlopJpFl%2bvlGj6Xeekt8fA8%3d&risl=&pid=ImgRaw&r=0'
-            }
-          />
-        </div>
-      </Carousel>
-      <h1>Thông tin chi tiết:</h1>
-      <h3>
-        <Mockdata />
-      </h3>
-      <h1>Comment:</h1>
-      <NoteCmt/>
-      <Footer>Footer</Footer>
-    </Layout>
+function Product_detail(){
 
-    
-  </div>,
-  document.getElementById('container'),
-);
-         
+
+  return(
+    <div>
+      <Layout style={{ background: 'white' }}>
+        <Header>Header</Header>
+
+        <p style={{ textAlign: 'center', fontSize: '30px' }}>Cho thuê phòng trọ chung cư có máy lạnh giá rẻ</p>
+
+        {/* Carousel */}
+        <div className="slider"  >
+          <Slide />
+        </div>
+        {/* Đổ dữ liệu vào table */}
+        <div style={{ border: '10px solid white', margin: '100px 100px', lineHeight: '10xp' }}>
+          <div className="mockdata">
+            <Mockdata />
+          </div>
+          {/* Đổ dự liệu văn bản */}
+          <div>
+            <h4>Thông tin chi tiết:</h4>
+          </div>
+          {/* Hiển thị danh sách các phòng tương thích */}
+          <div>
+            <h4>Những phòng tương thích</h4>
+            <Involve />
+          </div>
+          {/* Comment */}
+          <div>
+            <h4>Comment:</h4>
+            <NoteCmt />
+          </div>
+        </div>
+
+        <Footer>Footer</Footer>
+      </Layout>
+
+
+    </div>
+
+  );
+}
+export default Product_detail
